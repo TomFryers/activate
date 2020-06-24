62db91d16db4cd4804d846957472fe826db2164f
Tom Fryers <t.fryers@gmail.com>
2020-06-24 23:13:01 +0100

    Add support for GPX files without namespaces

--- load_gpx.py
+++ load_gpx.py
@@ -4,23 +4,25 @@ import xml.etree.ElementTree
 import track
 import times
 
-NAMESPACE = "{http://www.topografix.com/GPX/1/1}"
 
 FIELDS = {
     "lat": lambda p: float(p.get("lat")),
     "lon": lambda p: float(p.get("lon")),
-    "ele": lambda p: float(p.find(f"./{NAMESPACE}ele").text),
-    "time": lambda p: times.from_GPX(p.find(f"./{NAMESPACE}time").text),
+    "ele": lambda p: float(p.find("./ele").text),
+    "time": lambda p: times.from_GPX(p.find("./time").text),
 }
 
 
 def load_gpx(filename):
-    tree = xml.etree.ElementTree.parse(filename).getroot()
+    tree = xml.etree.ElementTree.iterparse(open(filename, "r"))
+    for _, element in tree:
+        _, _, element.tag = element.tag.rpartition("}")
+    tree = tree.root
     try:
-        name = tree.find(f"./{NAMESPACE}trk/{NAMESPACE}name").text
+        name = tree.find("./trk/name").text
     except AttributeError:
         name = "[No Name]"
-    points = tree.findall(f"./{NAMESPACE}trk/{NAMESPACE}trkseg/{NAMESPACE}trkpt")
+    points = tree.findall("./trk/trkseg/trkpt")
     fields = {}
     for point in points:
         for field in FIELDS:
