961345958f691124a569651892403f98d960f1f7
Tom Fryers <t.fryers@gmail.com>
2020-07-22 21:38:18 +0100

    Simplify file names for files starting with '_'

--- files.py
+++ files.py
@@ -1,4 +1,6 @@
 """Functions for manipulating files."""
+
+
 def has_extension(filename, extension) -> bool:
     """Determine if a file path has a given extension."""
     return filename.casefold().endswith("." + extension)
@@ -10,18 +12,22 @@ def encode_name(filename, current_filenames, directory):
 
     Renames foo.gpx to _0_foo.gpx if foo.gpx already exists. If that
     exists tries _1_foo.gpx, _2_foo.gpx... until a free name is found.
-    If the name already starts with an underscore, it is replaced with a
-    double underscore, so _foo_bar.gpx becomes __foo_bar.gpx,
-    _0___foo_bar.gpx etc.
+    If the name already starts with an underscore, the bare name cannot
+    be used, _foo_bar.gpx becomes _0__foo_bar.gpx, _1__foo_bar.gpx etc.
     """
-    if filename[0] == "_":
-        filename = "_" + filename
-    full_name = f"{directory}/{filename}"
+    # No-underscore, unique filename
+    if filename[0] != "_":
+        full_name = f"{directory}/{filename}"
+        if full_name not in current_filenames:
+            return full_name
+
+    # Others
     i = 0
-    while full_name in current_filenames:
+    while True:
         full_name = f"{directory}/_{i}_{filename}"
         i += 1
-    return full_name
+        if full_name not in current_filenames:
+            return full_name
 
 
 def decode_name(filename):
@@ -29,6 +35,4 @@ def decode_name(filename):
     if filename[0] != "_":
         return filename
     filename = filename[filename[1:].index("_") + 2 :]
-    if filename.startswith("__"):
-        return filename[2:]
     return filename
