4ad7df52bae4ae3c033211d970af87b4f2a7bbb6
Tom Fryers <t.fryers@gmail.com>
2020-08-09 11:35:08 +0100

    Add large photo viewer

--- activate/app/photos.py
+++ activate/app/photos.py
@@ -1,5 +1,13 @@
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
+
+
+class ClickablePhoto(QtWidgets.QLabel):
+    clicked = QtCore.pyqtSignal(int)
+
+    def mousePressEvent(self, event):
+        self.clicked.emit(self.parent().layout().indexOf(self))
+        super().mousePressEvent(event)
 
 
 class PhotoList(QtWidgets.QWidget):
@@ -22,6 +30,7 @@ class PhotoList(QtWidgets.QWidget):
     def replace_photos(self, filenames):
         """Replace the photos with new ones."""
         self.empty()
+        self.filenames = filenames
         if not filenames:
             return
         self.photos = [QtGui.QPixmap(f) for f in filenames]
@@ -38,7 +47,48 @@ class PhotoList(QtWidgets.QWidget):
         ]
         self.labels = []
         for photo in self.photos:
-            label = QtWidgets.QLabel(self)
+            label = ClickablePhoto(self)
             label.setPixmap(photo)
+            label.clicked.connect(self.show_photos)
             self.layout().addWidget(label)
             self.labels.append(label)
+
+    def show_photos(self, index):
+        viewer = PhotoViewer(self.filenames, index)
+        viewer.exec()
+
+
+class PhotoViewer(QtWidgets.QDialog):
+    def __init__(self, photos, current_index, *args, **kwargs):
+        self.current_index = current_index
+        self.photos = photos
+        super().__init__(*args, **kwargs)
+        self.label = QtWidgets.QLabel(self)
+        self.main_layout = QtWidgets.QHBoxLayout(self)
+        self.main_layout.addWidget(self.label)
+        self.show_photo()
+
+    def show_photo(self):
+        photo = self.photos[self.current_index]
+        if not isinstance(photo, QtGui.QPixmap):
+            photo = QtGui.QPixmap(photo)
+            photo = photo.scaled(
+                self.height(), self.width(), Qt.KeepAspectRatio, Qt.SmoothTransformation
+            )
+            self.photos[self.current_index] = photo
+        self.label.setPixmap(photo)
+
+    def set_new_index(self, index):
+        self.current_index = index
+        self.current_index %= len(self.photos)
+        self.show_photo()
+
+    def keyPressEvent(self, event):
+        print(event.key())
+        if event.key() in {Qt.Key_Right, Qt.Key_Space}:
+            self.set_new_index(self.current_index + 1)
+            return
+        if event.key() in {Qt.Key_Left, Qt.Key_Backspace}:
+            self.set_new_index(self.current_index - 1)
+            return
+        super().keyPressEvent(event)
