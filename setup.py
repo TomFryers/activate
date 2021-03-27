9c98b9edc0f24beccd90f011d42f827a7630298e
Tom Fryers <t.fryers@gmail.com>
2021-03-27 11:53:24 +0000

    Mark Python 3.7 as minimum version
    This was always required for dataclasses

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
